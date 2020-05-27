00ab71f3df19940273c1490ac42cea9bbc47e772
mubeenghauri <p176107@nu.edu.pk>
2020-05-28 00:44:53 +0500

    Added Class based views
    Changed previously used function based views to class based views.

--- masite/blog/views.py
+++ masite/blog/views.py
@@ -1,31 +1,38 @@
 import sys
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_list(request):
-    """ View Logic to display posts """
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)  # 3 post in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range, deliver last page
-        posts = paginator.page(paginator.num_pages)
-    print(f''"[VIEW:post list] found posts to be {posts}", file=sys.stderr)
-    # if not posts:
-    #     print("[*] Hmmm, found no post ?", file=sys.stderr)
-    # else:
-    #     print("[*] Yay, found posts")
-    return render(request, 'blog/post/list.html', {'page': page,
-                                                   'posts': posts})
+# Replacing function with class based views.
+# def post_list(request):
+#     """ View Logic to display posts """
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 post in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range, deliver last page
+#         posts = paginator.page(paginator.num_pages)
+#     print(f''"[VIEW:post list] found posts to be {posts}", file=sys.stderr)
+#     # if not posts:
+#     #     print("[*] Hmmm, found no post ?", file=sys.stderr)
+#     # else:
+#     #     print("[*] Yay, found posts")
+#     return render(request, 'blog/post/list.html', {'page': page,
+#                                                    'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
